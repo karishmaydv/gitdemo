bbe44f24e3b2170fa85a0e900650cc2a37c5f073
karishma <karishmaydv05@gmail.com>
2026-03-04 16:29:48 +0530

    negative test case and secure area task

--- assignmentlogin.py
+++ assignmentlogin.py
@@ -27,11 +27,39 @@ driver.find_element(By.ID,"password").send_keys("Password123")
 driver.find_element(By.ID,"submit").click()
 #for logout button visibility without wait
 
-logout_button = driver.find_element(By.LINK_TEXT, "Log out")
-#logout_button= driver.find_element(By.CLASS_NAME,"wp-block-button__link has-text-color has-background has-very-dark-gray-background-color")
+logout_button = driver.find_element(By.LINK_TEXT, "Log out") # link is present..
 
+# with explict wait for logut button visibility for specific logout button
+wait = WebDriverWait(driver, 10)
+Logout_button =wait.until(EC.visibility_of_element_located((By.LINK_TEXT, "Log out")))
+
+# Confirm it is displayed
 if logout_button.is_displayed():
     print("Logout button is visible")
-    # with explict wait for logut button visibility
 
-wait =WebDriverWait(driver,10).until(EC.visibility_of_element_located((By.LINK_TEXT, "Log out")))
+#2.Validate redirection to the secure area
+#validate using url
+#explicit wait used
+
+wait = WebDriverWait(driver, 10)
+
+# Wait until URL contains expected text
+wait.until(EC.url_contains("logged-in-successfully"))
+
+current_url = driver.current_url
+
+if "logged-in-successfully" in current_url:
+    print("✅ Successfully redirected to Secure Area")
+else:
+    print("❌ Redirection failed")
+
+#3. validate negative tc with invalid creds
+driver.get("https://practicetestautomation.com/practice-test-login/")
+driver.find_element(By.ID,"username").send_keys("karishma")
+driver.find_element(By.ID,"password").send_keys("123")
+driver.find_element(By.ID,"submit").click()
+wait = WebDriverWait(driver,5)
+#validate error message
+error_message =wait.until(EC.visibility_of_element_located((By.ID,"error")))
+if error_message.is_displayed():
+    print("Your username is invalid!")
